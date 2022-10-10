70a8b36f53b3a49ed64900d5eb8dccacd8dde049
Renato <renato.melo@ab-inbev.com>
2022-10-09 22:10:48 -0300

    feat(3/3): get external data with axios and fetch, using optional chaining

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 import './App.css'
 import {
   Button,
@@ -7,17 +7,36 @@ import {
   Image,
   Text
 } from './components'
-import cards from './db'
+import axios from 'axios'
 
 export default function App() {
   const [cardList, setCardList] = useState([])
 
-  useEffect(() => {
-    if (cards.length) {
+  // async function fetchData() {
+  //   const response = await fetch('http://localhost:3000/cards')
+  //   const cards = await response.json()
+
+  //   // This is just to show the conditional rendering
+  //   setTimeout(() => {
+  //     setCardList(cards)
+  //   }, 2000);
+  // }
+
+  async function fetchData() {
+    const response = await axios('http://localhost:3004/cards')
+    const cards = response.data
+
+    // This is just to show the conditional rendering
+    setTimeout(() => {
       setCardList(cards)
-    }
+    }, 2000);
+  }
+
+  useEffect(() => {
+    fetchData()
   }, [])
 
+  // comment cards constant content to show this
   if (!cardList.length) return "No cards available."
 
   return (
@@ -25,7 +44,8 @@ export default function App() {
       {cardList.map(card => {
         return (
           <Card id={card.type} key={card.id}>
-            <Image src={card.image} alt={card.alternative} />
+            {/* Remove optional chaining to check the error */}
+            <Image src={card.image?.src} alt={card.image?.alternative} />
             <Text element="h2">{card.name}</Text>
             <Text element="p">{card.description}</Text>
             <Button className={card.type}>Learn More</Button>
