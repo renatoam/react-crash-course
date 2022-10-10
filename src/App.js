670e1872dd884159b03a5f5a2ed8f3c4cc4fa26d
Renato <renato.melo@ab-inbev.com>
2022-10-09 21:56:06 -0300

    feat(3/1): create a dynamic list with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import './App.css'
 import {
   Button,
@@ -7,38 +6,23 @@ import {
   Image,
   Text
 } from './components'
+import cards from './db'
 
 export default function App() {
-  useEffect(() => {
-    console.log('Hooks are very useful!')
-  }, [])
+  if (!cards.length) return "No cards available."
 
   return (
     <Container>
-      <Card id="sedans">
-        <Image src="/images/icon-sedans.svg" alt="Sedans Icon" />
-        <Text element="h2">Sedans</Text>
-        <Text element="p">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</Text>
-        <Button className="sedans">
-          Learn More
-        </Button>
-      </Card>
-      <Card id="suvs">
-        <Image src="/images/icon-suvs.svg" alt="Suvs Icon" />
-        <Text element="h2">Suvs</Text>
-        <Text element="p">Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</Text>
-        <Button className="suvs">
-          Learn More
-        </Button>
-      </Card>
-      <Card id="luxury">
-        <Image src="/images/icon-luxury.svg" alt="Luxury Icon" />
-        <Text element="h2">Luxury</Text>
-        <Text element="p">Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.</Text>
-        <Button className="luxury">
-          Learn More
-        </Button>
-      </Card>
+      {cards.map(card => {
+        return (
+          <Card id={card.type} key={card.id}>
+            <Image src={card.image} alt={card.alternative} />
+            <Text element="h2">{card.name}</Text>
+            <Text element="p">{card.description}</Text>
+            <Button className={card.type}>Learn More</Button>
+          </Card>
+        )
+      })}
     </Container>
   )
 }
