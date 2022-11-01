8cae75585bad21268eeb9748bb39ed9f5af94a75
Renato <renato.melo@ab-inbev.com>
2022-11-01 11:17:44 -0300

    feat(app): change cra to vite

--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,7 @@ const router = require('./routes')
 const server = express()
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://127.0.0.1:5173',
   credentials: true
 }
 
