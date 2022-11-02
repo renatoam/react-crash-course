995558b42c4ad55e5dd5b60bac1291ffc44d4fb0
Renato <renato.melo@ab-inbev.com>
2022-11-02 19:03:38 -0300

    refactor: prevent to go to login when already logged in

--- src/components/layout/components/login/Login.jsx
+++ src/components/layout/components/login/Login.jsx
@@ -1,17 +1,20 @@
 import { Login as LoginIcon, Logout } from "@icon-park/react"
+import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
-import { useAuthContext } from "../../../../context/authContext"
 import './Login.scss'
+import Text from "../../../text/Text"
  
 export default function Login() {
-  const { user } = useAuthContext()
+  const user = useSelector(state => state.user)
   const Icon = user ? Logout : LoginIcon
+  const textContent = user ? 'Logout' : 'Login'
 
   return (
-    <button className="login">
+    <section role="button" className="login">
       <Link to="/auth">
+        <Text small>{textContent}</Text>
         <Icon />
       </Link>
-    </button>
+    </section>
   )
 }
--- src/hooks/useAuthenticate.js
+++ src/hooks/useAuthenticate.js
@@ -1,25 +1,26 @@
 import { useCallback, useEffect, useRef } from "react"
-import { useDispatch } from "react-redux"
-import { useNavigate } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { useLocation, useNavigate } from "react-router-dom"
 import { useAuthContext } from "../context/authContext"
 import { useFormContext } from "../context/formContext"
+import { useNotificationContext } from "../context/notificationContext"
 import { setUser } from "../store/userSlice"
 import useValidation from "./useValidation"
 
 const useAuthenticate = () => {
   const { loading, setLoading, setFormError } = useFormContext()
   const { authenticate } = useAuthContext()
+  const { notify } = useNotificationContext()
   const dispatch = useDispatch()
   const { emailValidation } = useValidation()
+  const user = useSelector(state => state.user)
 
   const email = useRef(null)
   const form = useRef(null)
 
   const navigate = useNavigate()
-
-  useEffect(() => {
-    email.current.focus()
-  }, [])
+  const location = useLocation()
+  const from = location.state?.from?.pathname || '/'
 
   const handleClearForm = () => {
     email.current.focus()
@@ -58,6 +59,17 @@ const useAuthenticate = () => {
     setFormError(true)
     setLoading(false)
   }
+
+  useEffect(() => {
+    email.current.focus()
+  }, [])
+
+  useEffect(() => {
+    if (user?.isLogged) {
+      notify('info', 'Você já está logado')
+      return navigate(from, { replace: true })
+    }
+  }, [user, from, navigate])
 
   return {
     form,
